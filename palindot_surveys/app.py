0c1842f1deb6887492527839c2985eab1582dbbb
Carlos Bolívar <cebemind@hotmail.com>
2024-02-25 18:45:04 -0400

    adding front

--- palindot_surveys/app.py
+++ palindot_surveys/app.py
@@ -4,12 +4,12 @@ import string
 import requests
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with your secret key
+app.secret_key = 'whatever'  # Replace with your secret key
 
 def generate_string(length=10):
     """Generate a random string of fixed length."""
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for _ in range(length))
+    url_chars = string.ascii_letters + string.digits + "$-_.+!*'()"
+    return ''.join(random.choice(url_chars) for _ in range(length))
 
 def generate_palindrome(length=10):
     """Generate a random palindromic string of fixed length."""
@@ -18,12 +18,18 @@ def generate_palindrome(length=10):
     second_half = first_half[::-1] if length % 2 == 0 else first_half[-1::-1]
     return first_half + second_half
 
+def randomize_order():
+    """Randomly return either generate_string() or generate_palindrome() first."""
+    funcs = [generate_string(), generate_palindrome()]
+    random.shuffle(funcs)
+    return funcs
+
 @app.route('/', methods=['GET', 'POST'])
 def survey():
     if request.method != 'POST':
         questions = ['Question 01', 'Question 02', 'Question 03', 'Question 04', 'Question 05', 'Question 06', 'Question 07', 'Question 08', 'Question 09', 'Question 10']
         # Generate a new string and a new palindrome for each question
-        session['questions_with_strings'] = {q: [generate_string(), generate_palindrome()] for q in questions}
+        session['questions_with_strings'] = {q: randomize_order() for q in questions}
         print("Session: ")
         print(session['questions_with_strings'])
 
@@ -44,7 +50,7 @@ def survey():
         response = requests.post(make_webhook_url, json=data_for_webhook)
         # Generate a new string and a new palindrome for each question
         questions = ['Question 01', 'Question 02', 'Question 03', 'Question 04', 'Question 05', 'Question 06', 'Question 07', 'Question 08', 'Question 09', 'Question 10']
-        session['questions_with_strings'] = {q: [generate_string(), generate_palindrome()] for q in questions}
+        session['questions_with_strings'] = {q: randomize_order() for q in questions}
 
     return render_template('survey.html', questions=session.get('questions_with_strings', {}))
 
